911e66ca0182758c683be91329aaa1150d7da1cb
JRH89 <thegemlins@gmail.com>
2023-01-25 05:07:31 -0800

    ok
    ok

---
+++ Enemies/ArrowTrap.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private GameObject[] fireballs;
+    private float cooldownTimer;
+
+
+    //attack
+    private void Attack()
+    {
+        cooldownTimer = 0;
+
+        fireballs[FindFireball()].transform.position = firePoint.position;
+        fireballs[FindFireball()].GetComponent<EnemyProjectile>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindFireball()
+    {
+        for (int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+                return i;
+        }
+        return 0;
+    }
+
+    //update
+    private void Update()
+    {
+        cooldownTimer += Time.deltaTime;
+
+        if (cooldownTimer >= attackCooldown)
+            Attack();
+    }
+
+
+
+
+
+
+}
---
+++ Enemies/EnemyProjectile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : EnemyDamage //Will damage the player every time they touch it
+{
+    public void SetDirection(float _direction)
+    {
+
+    }
+}
