acfb265fdab54aec161bbc7622974e72c903e5e4
JRH89 <thegemlins@gmail.com>
2023-01-26 00:17:08 -0800

    ok

--- Enemy/MeleeEnemy.cs
+++ Enemy/MeleeEnemy.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class MeleeEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float attackCooldown;
+    [SerializeField] private float range;
+    [SerializeField] private float colliderDistance;
+    [SerializeField] private int damage;
+    [SerializeField] private BoxCollider2D boxCollider;
+    [SerializeField] private LayerMask playerLayer;
+    private float cooldownTimer = Mathf.Infinity;
+    private Animator anim;
+
+    private Health playerHealth;
+
+    private void Awake()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only if player is in sight
+        if(PlayerInSight())
+        {
+
+         if (cooldownTimer >= attackCooldown)
+            {
+                cooldownTimer = 0;
+                anim.SetTrigger("meleeAttack");
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = 
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerLayer);
         
+        if(hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Health>();
+        
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
     }
 }
